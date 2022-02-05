34e0497cadb8c9b779c1589a2057bed2a2da817e
Barrel <visvam64@gmail.com>
2022-02-05 10:19:53 -0500

    Commiting for the first time?

---
+++ ClawCode/src/main/cpp/DalekDrive.cpp
@@ -0,0 +1,43 @@
+#include "Fermat.h"
+
+DalekDrive::DalekDrive() {
+	m_left[FRONT]  = new WPI_TalonFX(leftFront);
+	m_left[REAR]   = new WPI_TalonFX(leftRear);
+	m_right[FRONT] = new WPI_TalonFX(rightFront);
+	m_right[REAR]  = new WPI_TalonFX(rightRear);
+	//m_left[FRONT]->ConfigSelectedFeedbackSensor(FeedbackDevice.IntegratedSensor);
+}
+
+double
+DalekDrive::squareInput(double v)
+{
+	if (v < 0.0) {
+		return -(v * v);
+	}
+	return (v * v);
+}
+
+void
+DalekDrive::TankDrive(double l, double r, bool squaredInputs)
+{
+	if(squaredInputs) {
+		l = squareInput(l);
+		r = squareInput(r);
+	}
+	m_left[FRONT]->Set(l * MAX_SPEED * -1.0);
+	m_left[REAR]->Set(l * MAX_SPEED * -1.0);
+	m_right[FRONT]->Set(r * MAX_SPEED);
+	m_right[REAR]->Set(r * MAX_SPEED);
+}
+
+void
+DalekDrive::TankDrive(Joystick* leftStick, Joystick* rightStick, bool squaredInputs)
+{
+	TankDrive(leftStick->GetY(), rightStick->GetY(), squaredInputs);
+}
+
+void
+DalekDrive::TankDrive(Joystick& leftStick, Joystick& rightStick, bool squaredInputs)
+{
+		TankDrive(leftStick.GetY(), rightStick.GetY(), squaredInputs);
+}
---
+++ ClawCode/src/main/include/DalekDrive.h
@@ -0,0 +1,39 @@
+#include "Fermat.h"
+
+#pragma once
+
+#define WHEEL_CIRCUMFERENCE			0.612775 // meters
+#define GEAR_RATIO					12
+#define CAN_TIMEOUT					100
+#define STALL_LIMIT					60
+#define FREE_LIMIT					20
+#define RAMP_RATE					0.5
+#define MAX_SPEED					0.25
+#define NUM_MOTORS_PER_SIDE			2
+#define LEFT						0
+#define RIGHT						1
+#define FRONT						0
+#define REAR						1
+
+using namespace frc;
+using namespace rev;
+
+class DalekDrive {
+	public:
+		DalekDrive();
+		double squareInput(double v);
+		void TankDrive(Joystick* leftStick, Joystick* rightStick, bool squaredInputs);
+		void TankDrive(Joystick& leftStick, Joystick& rightStick, bool squaredInputs);
+		void TankDrive(double l, double r, bool squaredInputs);
+	
+	private:
+		enum MotorCount {
+			leftFront = 0,
+			leftRear = 1,
+			rightFront = 2,
+			rightRear = 3
+		};
+
+		WPI_TalonFX *m_left[2];
+		WPI_TalonFX *m_right[2];
+};
