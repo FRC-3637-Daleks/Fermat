78a4c6928c0f8541209827e964f511c68bf3cc70
robobruser <paulbruzzi@gmail.com>
2022-02-17 10:27:53 -0500

    swerve turn update

--- TestCode/src/main/cpp/RaspberryPi.cpp
+++ TestCode/src/main/cpp/RaspberryPi.cpp
@@ -5,11 +5,16 @@ RaspberryPi::RaspberryPi(DalekDrive *drive){
 }
 void RaspberryPi::SwerveTurn(double degrees, double distance) {
     SmartDashboard::PutNumber("Degrees",degrees-MAX_DEGREES); 
+    double speed = 0;
     if(distance > 24) {// track ball
         if(degrees < 0) {
-            m_drive->TankDrive((degrees-MAX_DEGREES)/10*MAX_SPEED * 0.75, (degrees-MAX_DEGREES)/10*MAX_SPEED, false);// swerve left while tracking ball
+            //speed = (degrees-MAX_DEGREES)/10*MAX_SPEED; //the orginal value that works
+            speed = fmod((distance / 48.0), 1) * (degrees/MAX_DEGREES) * MAX_SPEED; //me trying to make it better
+            m_drive->TankDrive(speed * 0.75, speed, false);// swerve left while tracking ball
         } else {
-            m_drive->TankDrive(-(degrees+MAX_DEGREES)/10*MAX_SPEED, -(degrees+MAX_DEGREES)/10*MAX_SPEED * 0.75, false);// swerve right while tracking ball
+            //speed = (degrees+MAX_DEGREES)/10*MAX_SPEED; //the orginal value that works
+            speed = fmod((distance / 48.0), 1) * (degrees/MAX_DEGREES) * MAX_SPEED; //me trying to make it better
+            m_drive->TankDrive(-1.0 * speed, -1.0 * speed * 0.75, false);// swerve right while tracking ball
         }
     }else{
         m_drive->StopLeft();
