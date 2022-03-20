4296f033a851e44eb00e69c3b515e6e5cb04042a
robobruser <paulbruzzi@gmail.com>
2022-03-20 00:50:55 -0400

    climb no bobbing

--- Fermat-Imported/src/main/cpp/Climb.cpp
+++ Fermat-Imported/src/main/cpp/Climb.cpp
@@ -25,22 +25,29 @@ Climb::Tick(){
     // frc::SmartDashboard::PutBoolean("Arm Pneumatics State", m_climb_solenoid->Get());
 
     if (CLIMB_SENSOR_TESTING == 0){
-        if (!(m_upperLimit->Get())){
+        // Maybe use adjust speed the more you use the axis
+        if(m_xbox->GetRawAxis(5)>0.5&&m_upperLimit->Get()){
             m_climb_motor->Set(CLIMB_MOTOR_SPEED);
-            m_climb_solenoid->Set(false);
-        } else if (!(m_lowerLimit->Get())){
+        } else if(m_xbox->GetRawAxis(5)<-0.5&&m_lowerLimit->Get()){
             m_climb_motor->Set(-CLIMB_MOTOR_SPEED);
-            m_climb_solenoid->Set(false);
-        } else {
-            if(m_xbox->GetRawAxis(5)>0.5){
-            m_climb_motor->Set(CLIMB_MOTOR_SPEED);
-            } else if(m_xbox->GetRawAxis(5)<-0.5){
-                m_climb_motor->Set(-CLIMB_MOTOR_SPEED);
-            } else{
-                //m_climb_motor->Set(m_climb_motor->Get()*-.9);
-                m_climb_motor->Set(0.0);
-            }
+        } else{
+            m_climb_motor->Set(0.0);
         }
+        // if (!(m_upperLimit->Get())){
+        //     m_climb_motor->Set(CLIMB_MOTOR_SPEED);
+        //     m_climb_solenoid->Set(false);
+        // } else if (!(m_lowerLimit->Get())){
+        //     m_climb_motor->Set(-CLIMB_MOTOR_SPEED);
+        //     m_climb_solenoid->Set(false);
+        // } else {
+        //     if(m_xbox->GetRawAxis(5)>0.5){
+        //     m_climb_motor->Set(CLIMB_MOTOR_SPEED);
+        //     } else if(m_xbox->GetRawAxis(5)<-0.5){
+        //         m_climb_motor->Set(-CLIMB_MOTOR_SPEED);
+        //     } else{
+        //         m_climb_motor->Set(0.0);
+        //     }
+        // }
     }
 
     if(m_xbox->GetLeftBumperPressed()){
