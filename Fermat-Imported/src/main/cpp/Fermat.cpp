8783e0e086eca0e57b711bb378b0b8ea91d1ee7c
MonkeyMan0508 <jluptonhill@gmail.com>
2022-04-04 18:25:25 -0400

    comp changes

--- Fermat-Imported/src/main/cpp/Fermat.cpp
+++ Fermat-Imported/src/main/cpp/Fermat.cpp
@@ -9,7 +9,10 @@ void Robot::RobotInit()
   // Only use these two lines if there is problems with the camera
   camera.SetResolution(160, 90);   
   camera.SetFPS(15);
-
+ // Get the USB camera from CameraServer
+    cs::UsbCamera camera = frc::CameraServer::StartAutomaticCapture();
+    // Set the resolution
+    camera.SetResolution(640, 480);
   m_xbox                = new frc::XboxController(XBOX);
   m_drive               = new DalekDrive(m_xbox);
   m_climb_solenoid      = new frc::Solenoid(PCM, PneumaticsModuleType::CTREPCM, CLIMB);
